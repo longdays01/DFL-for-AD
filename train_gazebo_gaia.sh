4dc20d0d3f468df70726c1ed8e5f01f5c990f8e7
longddaays <hlong112001@gmail.com>
2024-07-18 20:57:58 -0700

    update 18/7

--- train_gazebo_gaia.sh
+++ train_gazebo_gaia.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python main.py driving_gazebo --network_name gaia --architecture ring --model FADNet --n_rounds 1000 --bz_train 32 --bz_test 32 --device cpu --log_freq 40 --local_steps 1 --lr 0.0001 --decay sqrt
+python main.py driving_gazebo --network_name gaia --architecture ring --model FADNet --n_rounds 1000 --bz_train 32 --bz_test 32 --device cuda --log_freq 10 --local_steps 1 --lr 0.0001 --decay sqrt
